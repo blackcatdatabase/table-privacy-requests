90cb70ab1f9b744cfebfbccc4d9c59ce6ab12a90
Vito416 <vito416@protonmail.com>
2025-12-02 19:27:46 +0100

    Sync submodule state after YAML views + upsert/coverage fixes

--- schema/040_views_joins.mysql.sql
+++ schema/040_views_joins.mysql.sql
@@ -1,7 +1,7 @@
--- Auto-generated from joins-mysql.psd1 (map@mtime:2025-11-27T17:49:37Z)
+-- Auto-generated from joins-mysql.yaml (map@94ebe6c)
 -- engine: mysql
 -- view:   privacy_requests_status
--- Privacy requests status
+
 CREATE OR REPLACE ALGORITHM=TEMPTABLE SQL SECURITY INVOKER VIEW vw_privacy_requests_status AS
 SELECT
   type,
